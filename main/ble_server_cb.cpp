060cc6ec37b04ab4e93235f109f31ad23b192b2a
Jackson Ming Hu <huming2207@gmail.com>
2021-01-14 23:06:50 +1100

    BLE: implement a simple server callback

--- main/ble_server_cb.cpp
+++ main/ble_server_cb.cpp
@@ -1,5 +1,29 @@
-//
-// Created by hu on 12/1/21.
-//
-
 #include "ble_server_cb.hpp"
+
+void ble_server_cb::onConnect(NimBLEServer *server)
+{
+    ESP_LOGW(TAG, "On connect");
+}
+
+void ble_server_cb::onDisconnect(NimBLEServer *server)
+{
+    ESP_LOGW(TAG, "On disconnect");
+}
+
+bool ble_server_cb::onConfirmPIN(uint32_t pass_key)
+{
+    ESP_LOGW(TAG, "Got PIN: %u", pass_key);
+
+    return pass_key == 123456;
+}
+
+uint32_t ble_server_cb::onPassKeyRequest()
+{
+    ESP_LOGW(TAG, "On pass key request");
+    return 123456;
+}
+
+void ble_server_cb::onAuthenticationComplete(ble_gap_conn_desc *desc)
+{
+    ESP_LOGW(TAG, "Auth complete");
+}
--- main/includes/ble_server_cb.hpp
+++ main/includes/ble_server_cb.hpp
@@ -1,13 +1,18 @@
-//
-// Created by hu on 12/1/21.
-//
-
 #pragma once
 
+#include <NimBLEDevice.h>
 
-class ble_server_cb
+class ble_server_cb : public NimBLEServerCallbacks
 {
+public:
+    void onConnect(NimBLEServer *server) override;
+    void onDisconnect(NimBLEServer* server) override;
+    uint32_t onPassKeyRequest() override;
+    bool onConfirmPIN(uint32_t pass_key) override;
+    void onAuthenticationComplete(ble_gap_conn_desc *desc) override;
 
+private:
+    static constexpr const char *TAG = "ble_server";
 };
 
 
