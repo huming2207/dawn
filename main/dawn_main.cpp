b7508a90ad5377c2b9af8955bac15636dce49508
Jackson Ming Hu <huming2207@gmail.com>
2021-01-14 23:07:23 +1100

    Implement start-up code

--- main/dawn_main.cpp
+++ main/dawn_main.cpp
@@ -7,44 +7,18 @@
 #include "esp_log.h"
 
 #include "sk6812rgbw.hpp"
+#include <NimBLEDevice.h>
+#include "led_serv_mgr.hpp"
 
 #define TAG "dawn"
 
 extern "C" void app_main(void)
 {
     ESP_LOGI(TAG, "Hello world");
-    sk6812rgbw<GPIO_NUM_5, 15, RMT_CHANNEL_7> sk;
-    sk.init();
-    for (int idx = 0; idx < 15; idx++) {
-        switch(idx % 5) {
-            case 0: {
-                sk.set_color(idx, 0, 0, 0, 255, 255);
-                break;
-            }
-            case 1: {
-                sk.set_color(idx, 255, 0, 0, 0, 255);
-                break;
-            }
-            case 2: {
-                sk.set_color(idx, 0, 255, 0, 0, 255);
-                break;
-            }
-            case 3: {
-                sk.set_color(idx, 0, 0, 255, 0, 255);
-                break;
-            }
-            case 4: {
-                sk.set_color(idx, 255, 255, 0, 0, 255);
-                break;
-            }
 
-            default: {
-                sk.set_color(idx, 0, 0, 0, 255, 255);
-                break;
-            }
-        }
-    }
-    ESP_ERROR_CHECK(sk.commit());
+    NimBLEDevice::init("Bluart");
+
+    ESP_ERROR_CHECK(led_serv_mgr::instance().init());
 
     vTaskDelay(portMAX_DELAY);
 }
